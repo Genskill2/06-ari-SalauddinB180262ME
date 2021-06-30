8795a9c21c3fc1e38f0fe82eaf39b0535a7e1421
SalauddinB180262ME <84757830+SalauddinB180262ME@users.noreply.github.com>
2021-06-30 21:16:06 +0530

    Update ari.c

---
+++ ari.c
@@ -0,0 +1,50 @@
+string ari(string s)
+{
+  int  w, sent, c=0;
+  for( int i =0; i++; s[i]!=0)
+  {
+    if(islanum(s[i])
+      c++;
+     if(s[i] = 32)
+     w++;
+     if(s[i] = 46)
+     sent++;
+    }
+    float result = (4.71*c/w) + (0.5*w/s) - 21.43;
+     result = ceil(result);
+     if( result ==1)
+     return "Kindergarten";
+     if(result==2)
+     return "First/Second Grade";
+     if(result==3)
+     return "Third Grade";
+     if(result==4)
+      return "Fourth Grade";
+     if(result==5)
+    return "Fifth Grade";
+     if(result==6)
+      return"Sixth Grade";
+     if(result==7)
+     return "Seventh Grade";
+     if(result==8)
+     return " Eigth Grade";
+     
+     
+     if(result==9)
+     return "Ninth Grade";
+     
+     if(result==10)
+     return "Tenth Grade";
+     
+     if(result==11)
+     return "Eleventh Grade";
+     if(result==12)
+     return "Twelfth grade";
+     
+     if(result==13)
+     return "College student"; 
+     if(result==14)
+      return "Professor";
+      }
+     
+     
